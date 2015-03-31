3c9de2b2231fb04e6058bafee6508dd24a97cab2
stacca <caronstace@gmail.com>
2015-03-31 13:38:46 +0100

    working name parms

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -7,14 +7,16 @@ class BattleShips < Sinatra::Base
     erb :index
   end
 
-    get '/new_game' do
-      "What's your name?"
-      erb :game_start
-    end
+  get '/new_game' do
+    "What's your name?"
+    erb :game_start
+  end
 
-    get '/start' do
-      'hello'
-    end
+  get '/start' do
+    'hello'
+    @player = params[:name]
+    erb :start
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
